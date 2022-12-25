bf83b6130d348e9d4b1218b5f8ea0672390bc7d4
Anton <Anton.Rudakov216@mail.ru>
2022-12-25 15:50:13 +0300

    update

--- main.cpp
+++ main.cpp
@@ -1,60 +1,63 @@
-#include <iostream>  // Для std::cout
-#include <vector>    // Для std::vector
-#include <algorithm> // Для std::find_if
+#include <iostream> // Для std::cout
+#include <string>   // Для std::string
+#include <vector>   // Для std::vector
+#include <set>      // Для std::set
+#include <map>
+#include <sstream>
 
-// Task 1. Имеется отсортированный массив целых чисел. Необходимо разработать функцию
-// insert_sorted, которая принимает вектор и новое число и вставляет новое число в
-// определенную позицию в векторе, чтобы упорядоченность контейнера сохранялась.
-// Реализуйте шаблонную функцию insert_sorted, которая сможет аналогично работать с любым
-// контейнером, содержащим любой тип значения.
-template <typename Container, typename Type>
-void insert_sorted(Container& arr, const Type in){
-    auto it = std::find_if(arr.begin(), arr.end(), [in](int i){
-        return (in <= i);
-    });
-    arr.insert(it, in);
+// Task 1. Создать шаблонную функцию, которая принимает итераторы на начало и конец последовательности слов,
+// и выводящую в консоль список уникальных слов (если слово повторяется больше 1 раза, то вывести его надо один раз).
+// Продемонстрировать работу функции, передав итераторы различных типов.
+template<typename iterator>
+void printUniqueWords(iterator begin, iterator end){
+    std::set<std::string> out;
+    for(auto it = begin; it != end; ++it) out.insert(*it);
+    for(const auto& it : out) std::cout << it << " "; std::cout << std::endl;
 }
 
-#include <iterator>
-
-// Task 2. Сгенерируйте вектор a, состоящий из 100 вещественный чисел, представляющий собой
-// значения аналогового сигнала. На основе этого массива чисел создайте другой вектор целых
-// чисел b, представляющий цифровой сигнал, в котором будут откинуты дробные части чисел.
-// Выведите получившиеся массивы чисел. Посчитайте ошибку, которой обладает цифровой
-// сигнал по сравнению с аналоговым по формуле:
-// 𝑖=0; 𝑁−1; ∑ (𝑎𝑖 − 𝑏𝑖) ^ 2
-// где:
-// N - количество элементов в векторе,
-// a - элемент вектора с дробными числами,
-// b - элемент вектора с целыми числами
-// Постарайтесь воспользоваться алгоритмическими функциями, не используя циклы.
+// Task 2. Используя ассоциативный контейнер, напишите программу,
+// которая будет считывать данные введенные пользователем из стандартного потока ввода и разбивать их на предложения.
+// Далее программа должна вывести пользователю все предложения,
+// а также количество слов в них, отсортировав предложения по количеству слов.
 void Task2(){
-    const unsigned int SIZE = 10;
-    std::vector<double> a(SIZE, 0.0);
-    std::generate(a.begin(), a.end(), [](){ return double(rand()%1000)/double(10); });
-    std::cout << "Arr 'a': ";
-    for(const auto& it : a) std::cout << it << ", "; std::cout << std::endl;
-    std::vector<int> b;
-    std::copy(a.begin(), a.end(), std::back_inserter(b));
-    std::cout << "Arr 'b': ";
-    for(const auto& it : b) std::cout << it << ", "; std::cout << std::endl;
-    std::cout << "Result:  ";
-    for(auto i = 0; i < a.size(); ++i) std::cout << a[i] - b[i] << ", "; std::cout << std::endl;
+    std::string str;
+    std::getline(std::cin, str);
+    std::map<int, std::string> suggestions;
+    int index = 1; // Номер предложения
+    std::stringstream iss(str); // Весь текст
+
+    std::string suggestion("");
+    while(!iss.eof()){
+        std::string word;
+        iss >> word;
+        suggestion+=word;
+        suggestion+=" ";
+        if(word.find('.') != std::string::npos){
+            suggestions.insert({ index++ , suggestion });
+            suggestion = "";
+        }
+    }
+    std::cout << std::endl;
+    for(const auto& it : suggestions) std::cout << it.first << ": " << it.second << std::endl;
 }
 
 int main(){
     
     // Task 1 ====================
     std::cout << "Task 1:" << std::endl;
-    std::vector<int> array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    for(const auto& it : array) std::cout << it << ' '; std::cout << std::endl;
-    insert_sorted(array, 5);
-    for(const auto& it : array) std::cout << it << ' '; std::cout << std::endl;
-    std::cout << std::endl << std::endl;
+    std::vector<std::string> vec = { "test", "test1", "test2", "test", "test1"};
+    std::cout << "The original words: ";
+    for(const auto& it : vec) std::cout << it << " ";
+    std::cout << "\nThe unique words:   ";
+    printUniqueWords(vec.begin(), vec.end());
+    std::cout << std::endl;
     // ===========================
 
     // Task 2 ====================
     std::cout << "Task 2:" << std::endl;
+    // Предполагается, что обозначением конца строки является только точка.
+    // Тестовый текст для проверки:
+    // Advertisers study how people learn so that they can 'teach' them to respond to their advertising. They want us to be interested, to try something, and then to do it again. These are the elements of learning: interest, experience and repetition. If an advert can achieve this, it is successful. If an advert works well, the same technique can be used to advertise different things. So, for example, in winter if the weather is cold and you see a family having a warming cup of tea and feeling cosy, you may be interested and note the name of the tea ... Here the same technique is being used as with the cool, refreshing drink.
     Task2();
     std::cout << std::endl;
     // ===========================
